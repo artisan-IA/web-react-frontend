fb2ee6bccaac2f5db4ae9914c8caf8bcb5000cf8
Gerard Guasch <haldawe@hotmail.com>
2023-05-31 11:26:30 +0200

    first jsx iteration of sidebar done, missing CSS

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,56 @@
+import { 
+  LuSlidersHorizontal, 
+  BsFillBarChartFill,
+  BiLineChart,
+  BsMap,
+  FaBars 
+} from "react-icons"
+import { useState } from "react"
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
+
+  const [sidebarActive, setSidebarActive] = useState(false);
+  const toggle = () => setSidebarActive(!sidebarActive);
 
   const sections = [
     { 
-      name: "slider",
+      name: "Slider",
       link: "/slider",
-      icon: ""
+      icon: <LuSlidersHorizontal/>
     },
     { 
-      name: "bar chart",
+      name: "Bar chart",
       link: "/barChart",
-      icon: ""
+      icon: <BsFillBarChartFill/>
     },
     { 
-      name: "line chart",
+      name: "Line chart",
       link: "/lineChart",
-      icon: ""
+      icon: <BiLineChart/>
     },
     { 
-      name: "map",
+      name: "Map",
       link: "/map",
-      icon: ""
+      icon: <BsMap/>
     }
-  ]
+  ];
 
   return (
-    <div>
-
+    <div id="dashboard-sidebar">
+      {sections.map((section, i) => {
+        return (
+          <div>
+            <div className="section-toggle">
+              <FaBars onClick={toggle} />
+            </div>
+            <NavLink to={section.link} key={i} className="section-link">
+              <div className="section-icon">{section.icon}</div>
+              <div style={{ display: sidebarActive ? "block" : "none"}} className="section-name">{section.name}</div>
+            </NavLink>
+          </div>
+        )
+      })}
     </div>
   )
 };
